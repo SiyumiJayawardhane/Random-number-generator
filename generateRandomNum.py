de1ca113c8a882741e1b8936aa5de3555e9cd2a2
Siyumi  Jayawardhane <139908557+SiyumiJayawardhane@users.noreply.github.com>
2024-08-20 18:57:27 +0530

    Add files via upload

---
+++ generateRandomNum.py
@@ -0,0 +1,6 @@
+import random
+maxvalue=0
+minvalue=0
+maxvalue=int(input("Enter a max value:"))
+minvalue=int(input("Enter a min value:"))
+print("Random number between max number and min number:",random.randint(minvalue,maxvalue))
